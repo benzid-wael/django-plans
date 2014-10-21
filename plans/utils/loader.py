0f9ffc0d97ef292b6a618891737434b012ee15c6
Wael BEN ZID <benzid.wael@hotmail.fr>
2014-10-21 09:47:53 +0100

    Update doc of load_class utility

--- plans/utils/loader.py
+++ plans/utils/loader.py
@@ -3,7 +3,7 @@
 
 def load_class(name):
     """
-    load a class by dotted path.
+    load a class by dotted path if possible, or raise ImportError.
     """
     parts = name.split('.')
     module = __import__('.'.join(parts[0:-1]), globals(), locals(), [parts[-1]])
