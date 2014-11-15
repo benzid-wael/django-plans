ad8218ff6f058ede16d551b27ab4e6a17d538995
Wael BEN ZID <benzid.wael@hotmail.fr>
2014-11-15 15:24:46 +0100

    Missing changeset in last commit added

--- tests/credit_card.py
+++ tests/credit_card.py
@@ -4,4 +4,7 @@ from plans.utils.credit_card import CreditCard, Visa
 
 
 visa_card = Visa("John Doe", "4111111111111111", "111", 2090, 12)
+expired_visa = Visa("John Doe", "4111111111111111", "111", 1990, 12)
 expired_card = CreditCard("John Doe", "4111111111111111", "111", 1990, 12)
+
+unsupported_card = CreditCard("John Doe", "9991111111111111", "111", 1990, 12)
