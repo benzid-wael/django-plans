94638e851448ec17fe948c313ec32d68f9e028f7
Wael BEN ZID <benzid.wael@hotmail.fr>
2014-12-03 22:45:00 +0100

    Improve load_class function's documentation

--- plans/utils/loader.py
+++ plans/utils/loader.py
@@ -3,7 +3,8 @@
 
 def load_class(name):
     """
-    load a class by dotted path if possible, or raise ImportError.
+    load the class defined with dotted path notation if possible,
+    or raise ImportError.
     """
     parts = name.split('.')
     module = __import__('.'.join(parts[0:-1]), globals(), locals(),
